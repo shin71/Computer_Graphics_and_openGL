b9b6ece9d4e7ffbd2dbc3f652d69aacab40c8bf7
shin71 <88245670+shin71@users.noreply.github.com>
2022-09-08 11:45:12 +0530

    Update two_triangles_using_different_VBOS_AND_VAOS.cpp

--- OpenGL_techniques/two_triangles_using_different_VBOS_AND_VAOS.cpp
+++ OpenGL_techniques/two_triangles_using_different_VBOS_AND_VAOS.cpp
@@ -157,7 +157,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
+        // drawing our triangles
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO[0]); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
